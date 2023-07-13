6a11a6f9ba71438efd99efe67564efc5ea852bd1
Jordan Latter <Jlatter1111@gmail.com>
2023-07-13 18:58:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+questions_dict = dict()
+    #loop through questions and answers
+
+    #evaluate answers
+
+
+
+def run_quiz():
+    candidate = input("Enter your Name")
+   questions_dict
+
+
+
+
+
+def randomise_questions():
